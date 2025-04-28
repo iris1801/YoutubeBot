ded8ddee1510a917cc511b3a0061a7068cd2e1fe
iris1801 <44752281+iris1801@users.noreply.github.com>
2025-04-28 23:47:18 +0200

    Update test_opemvoice.py

--- scripts/scripts/test_opemvoice.py
+++
@@ -1,9 +0,0 @@
-from TTS.api import TTS
-import os
-
-os.makedirs("./temp", exist_ok=True)
-
-tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=False, gpu=False)
-tts.tts_to_file(text="Hello MindBloom, your bot is ready!", file_path="./temp/test_hello.wav")
-
-print("[+] Test audio generated: ./temp/test_hello.wav")
